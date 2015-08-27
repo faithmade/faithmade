8356242af442755b4a69d9a5f25167f69099043b
Anthony Garand <garand@garand.me>
2015-08-27 09:56:57 -0400

    Replace "WordPress" with "Faithmade"

--- faithmade.php
+++ faithmade.php
@@ -43,6 +43,96 @@ function faithmade_adminbar_css() {
 
 add_action( 'wp_enqueue_scripts', 'faithmade_adminbar_css' );
 
+function faithmade_replace_wordpress( $translated_text, $text, $domain ){
+
+    switch ( $translated_text ) {
+        case 'WordPress should correct invalidly nested XHTML automatically' :
+            $translated_text = __( 'Faithmade should correct invalidly nested XHTML automatically', 'faithmade' );
+
+        break;
+    }
+
+    return $translated_text;
+
+}
+
+add_filter( 'gettext', 'faithmade_replace_wordpress', 10, 3 );
+
+function faithmade_replace_wordpress_xhtml( $translated_text, $text, $domain ){
+
+    switch ( $translated_text ) {
+        case 'WordPress should correct invalidly nested XHTML automatically' :
+            $translated_text = __( 'Faithmade should correct invalidly nested XHTML automatically', 'faithmade' );
+
+        break;
+    }
+
+    return $translated_text;
+
+}
+
+add_filter( 'gettext', 'faithmade_replace_wordpress_xhtml', 10, 3 );
+
+function faithmade_replace_wordpress_comment_contains1( $translated_text, $text, $domain ){
+
+    switch ( $translated_text ) {
+
+        case 'When a comment contains any of these words in its content, name, URL, e-mail, or IP, it will be held in the <a href="edit-comments.php?comment_status=moderated">moderation queue</a>. One word or IP per line. It will match inside words, so &#8220;press&#8221; will match &#8220;WordPress&#8221;.':
+            $translated_text = __( 'When a comment contains any of these words in its content, name, URL, e-mail, or IP, it will be held in the <a href="edit-comments.php?comment_status=moderated">moderation queue</a>. One word or IP per line. It will match inside words, so &#8220;made&#8221; will match &#8220;Faithmade&#8221;.' );
+
+        break;
+    }
+
+    return $translated_text;
+
+}
+
+add_filter( 'gettext', 'faithmade_replace_wordpress_comment_contains1', 10, 3 );
+
+function faithmade_replace_wordpress_comment_contains2( $translated_text, $text, $domain ){
+
+    switch ( $translated_text ) {
+
+        case 'When a comment contains any of these words in its content, name, URL, e-mail, or IP, it will be marked as spam. One word or IP per line. It will match inside words, so &#8220;press&#8221; will match &#8220;WordPress&#8221;.';
+            $translated_text = __( 'When a comment contains any of these words in its content, name, URL, e-mail, or IP, it will be marked as spam. One word or IP per line. It will match inside words, so &#8220;made&#8221; will match &#8220;Faithmade&#8221;.' );
+
+        break;
+    }
+
+    return $translated_text;
+
+}
+
+add_filter( 'gettext', 'faithmade_replace_wordpress_comment_contains2', 10, 3 );
+
+function faithmade_replace_wordpress_permalink( $translated_text, $text, $domain ){
+
+    switch ( $translated_text ) {
+        case 'By default WordPress uses web <abbr title="Universal Resource Locator">URL</abbr>s which have question marks and lots of numbers in them; however, WordPress offers you the ability to create a custom URL structure for your permalinks and archives. This can improve the aesthetics, usability, and forward-compatibility of your links. A <a href="https://codex.wordpress.org/Using_Permalinks">number of tags are available</a>, and here are some examples to get you started.';
+            $translated_text = __( 'By default Faithmade uses web <abbr title="Universal Resource Locator">URL</abbr>s which have question marks and lots of numbers in them; however, Faithmade offers you the ability to create a custom URL structure for your permalinks and archives. This can improve the aesthetics, usability, and forward-compatibility of your links. A <a href="https://codex.wordpress.org/Using_Permalinks">number of tags are available</a>, and here are some examples to get you started.' );
+
+        break;
+    }
+
+    return $translated_text;
+
+}
+
+add_filter( 'gettext', 'faithmade_replace_wordpress_permalink', 10, 3 );
+
+function faithmade_remove_gf_menu_links() {
+    // remove_submenu_page( 'gf_edit_forms', 'gf_edit_forms' );
+    // remove_submenu_page( 'gf_edit_forms', 'gf_new_form' );
+    // remove_submenu_page( 'gf_edit_forms', 'gf_new_formf_help' );
+    // remove_submenu_page( 'gf_edit_forms', 'gf_entries' );
+    // remove_submenu_page( 'gf_edit_forms', 'gf_settings' );
+    // remove_submenu_page( 'gf_edit_forms', 'gf_export' );
+    remove_submenu_page( 'gf_entries', 'gf_update' );
+    remove_submenu_page( 'gf_entries', 'gf_addons' );
+    remove_submenu_page( 'gf_entries', 'gf_help' );
+}
+add_action( 'admin_menu', 'faithmade_remove_gf_menu_links', 9999 );
+
 /**
  * Remove admin bar links.
  */
