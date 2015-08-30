e11e44d13ac56ffdac423c48c46a7428654bc6b8
Chris Wallace <chris@liftux.com>
2015-08-30 18:33:03 -0500

    fix PHP warning in redirect function

--- faithmade.php
+++ faithmade.php
@@ -189,10 +189,17 @@ add_action( 'wp_before_admin_bar_render', 'faithmade_remove_admin_bar_links' );
 add_filter('login_redirect', 'custom_redirect_filter', 0, 3);
 
 function custom_redirect_filter($redirect_to, $request, $user) {
-    $user_blogs = get_blogs_of_user($user->ID);
-    foreach ($user_blogs as $user_blog) {
-        $user_blog->path;
-        return site_url($user_blog->path);
-    }
+
+    if( is_user_logged_in() ):
+
+        $user_blogs = get_blogs_of_user($user->ID);
+
+        foreach ($user_blogs as $user_blog) {
+            $user_blog->path;
+            return site_url($user_blog->path);
+        }
+
+    endif;
+
     return site_url();
 }
