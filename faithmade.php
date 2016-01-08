0d1e1bcfdefa5e8282e66089305bab413c248df9
Christian Chung <christian.m.chung@gmail.com>
2016-01-07 20:02:28 -0500

    Fixed hook

--- faithmade.php
+++ faithmade.php
@@ -152,4 +152,4 @@ add_action( 'init', 'faithmade_cache_control', 15 );
 function faithmade_enqueue_activate_styles() {
     wp_enqueue_script( 'faithmade-wp-activate-style', dirname( __FILE__ ) . '/activate.css' );
 }
-add_action( 'do_activate_header', 'faithmade_enqueue_activate_styles', 200 );
+add_action( 'activate_wp_head', 'faithmade_enqueue_activate_styles', 200 );
